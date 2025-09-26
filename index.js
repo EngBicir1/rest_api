ba928e9947230cb95b1a17b41ff40fff2846e5fc
Abdifatah-Mohamed <yuusufmoharaage@gmail.com>
2025-09-26 14:25:22 +0300

    commited

--- index.js
+++ index.js
@@ -5,5 +5,22 @@ app.get('/',(req,res)=>{
     res.send('hellow express');
 });
  
+const products =[
+    {
+        id:1,
+        name:'Orage',
+        price:20,
+    },
+    {
+        id:2,
+        name:'Apple',
+        price:30,
+    }
+]
+app.get("/products", (req,res)=>{
+    res.json(products);
+})
+
+
 
 app.listen(3000,()=> console.log('server is running at http://localhost:3000'));
